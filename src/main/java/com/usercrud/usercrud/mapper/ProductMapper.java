3720b0e349945440127a3e14d7fa42a27c4da645
haons211 <syhaoc2dh@gmail.com>
2024-07-16 21:50:03 +0700

    21.50

--- src/main/java/com/usercrud/usercrud/mapper/ProductMapper.java
+++ src/main/java/com/usercrud/usercrud/mapper/ProductMapper.java
@@ -5,11 +5,20 @@ import com.usercrud.usercrud.model.Products;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     ProductsDto ProductsToProductsDto(Products products);
     List<ProductsDto> ProductsToProductsDto(List<Products> products);
-    Products ProductsDtoToProducts(ProductsDto productsDto);
+   default Products ProductsDtoToProducts(ProductsDto productsDto){
+       java.util.Date date = new java.util.Date();
+       java.sql.Date sqlDate= new java.sql.Date(date.getTime());
+       Products product = new Products();
+       product.setTimestamp(sqlDate);
+return null;
+   };
 }
